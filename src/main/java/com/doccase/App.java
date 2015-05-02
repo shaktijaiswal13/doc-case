c8bcb017a6def6d4cf7779a312244ebb36849534
shaktikumar <shaktijaiswal13@gmail.com>
2015-05-02 21:36:19 +0100

    setting up mvn project

---
+++ src/main/java/com/doccase/App.java
@@ -0,0 +1,13 @@
+package com.doccase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
