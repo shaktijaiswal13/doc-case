c3c9f17f3b167c23c986520c9b21ec30a4e605fc
shaktikumar <shaktijaiswal13@gmail.com>
2015-05-02 22:13:26 +0100

    code for connection to mongodb

--- src/main/java/com/doccase/App.java
+++ src/main/java/com/doccase/App.java
@@ -1,13 +1,24 @@
 package com.doccase;
 
+import java.net.UnknownHostException;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		try {
+			MongoClient mongoDb = new MongoClient(new ServerAddress("localhost",
+					27017));
+			DB testDb = mongoDb.getDB("test");
+			System.out.println(testDb.getCollectionNames());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
 }
