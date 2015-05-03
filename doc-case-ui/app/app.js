6795ddb409650f3df0113a71b336e6a04cd1d171
“shakumar” <shakumar@guidewire.com>
2015-05-04 00:51:48 +0100

    ui angular

---
+++ doc-case-ui/app/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+}]);
