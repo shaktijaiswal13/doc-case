526a99cba2ea1234fce98975022e11903557403f
“shakumar” <shakumar@guidewire.com>
2015-05-10 22:28:28 +0100

    update create document

--- src/main/webapp/doc-case-ui/app/js/views/DocumentCreate/DocumentCreateController.js
+++ src/main/webapp/doc-case-ui/app/js/views/DocumentCreate/DocumentCreateController.js
@@ -14,15 +14,23 @@ angular.module('myApp.DocumentCreate', ['ngRoute'])
 
 .controller('documentCreateCtrl', ['$scope', '$http', 'fileUpload',
     function($scope, $http, fileUpload) {
-        $scope.saveDocument = function() {
-            uploadFile();
+        $scope.document = {
+            name: "Electricity bill",
+            description: "November electricity bill",
+            signed: true,
+            scanned: true,
+            coloured: true,
+            url: ""
+        }
+        $scope.saveDocument = function(document) {
+            uploadFile(document);
         };
-        $scope.uploadFile = function() {
+        $scope.uploadFile = function(document) {
             var file = $scope.myFile;
             console.log(file) //{webkitRelativePath: "", lastModified: 1430866129000, lastModifiedDate: Tue May 05 2015 23:48:49 GMT+0100 (IST), name: "Page 1&2.png", type: "image/png"…}
             console.log('file is ' + JSON.stringify(file));
             var uploadUrl = "./rest/file";
-            fileUpload.uploadFileToUrl(file, uploadUrl);
+            fileUpload.uploadFileToUrl(file, uploadUrl, document);
         };
     }
 ])
@@ -48,7 +56,7 @@ angular.module('myApp.DocumentCreate', ['ngRoute'])
 
 .service('fileUpload', ['$http',
     function($http) {
-        this.uploadFileToUrl = function(file, uploadUrl) {
+        this.uploadFileToUrl = function(file, uploadUrl, document) {
             var fd = new FormData();
             fd.append('file', file);
             $http.post(uploadUrl, fd, {
@@ -56,13 +64,13 @@ angular.module('myApp.DocumentCreate', ['ngRoute'])
                 headers: {
                     'Content-Type': undefined
                 }
-            }).success(function() {
-                console.log("File saved successfully....");
+            }).success(function(result) {
+                console.log("File saved successfully...." + result);
                 console.log("Saving document....");
-                var data = {
-                    name: "Electricity bill"
-                }
-                $http.post("./rest/document", data)
+                document.url = result.url
+                var data = []
+                data.push(document)
+                $http.post("./rest/documents", JSON.stringify(data))
                     .success(function() {
                         console.log("Document saved successfully....");
                     }).error(function(err) {
