00b4f87ab2ad207b0791e6655f7249fdd50d382f
“shakumar” <shakumar@guidewire.com>
2015-05-17 00:43:47 +0100

    updated search ui

--- src/main/webapp/doc-case-ui/app/js/views/DocumentsSearch/DocumentsSearchController.js
+++ src/main/webapp/doc-case-ui/app/js/views/DocumentsSearch/DocumentsSearchController.js
@@ -16,32 +16,34 @@ angular.module('myApp.DocumentsSearch', ['ngRoute'])
         $scope.model = {
             result: "not result"
         };
-
+        $scope.search = function(model) {
+            uploadFile(model);
+        };
 
         function displayResult(item) {
-            $('.alert').show().html('You selected <strong>' + item.value + '</strong>: <strong>' + item.text + '</strong>');
+            $('.alert').show().html('You selected <strong>' + item.value + '</strong>');
         }
 
         $("#name").typeahead({
             ajax: {
                 url: './rest/documents',
                 method: 'get',
-                triggerLength: 3,
+                triggerLength: 1,
                 preDispatch: function(query) {
                     console.log("predispatch query: " + query);
+                    $("#queryResult").text("");
                     return {
                         query: query
                     }
                 },
                 preProcess: function(data) {
+                    var transformedData = data.map(function(obj) {
+                        return obj.name + " " + obj.description;
+                    })
                     console.log("preProcess data: " + data);
-                    $("#queryResult").text(data);
-                    if (data.success === false) {
-                        // Hide the list, there was some error
-                        return false;
-                    }
+                    $("#queryResult").text(JSON.stringify(transformedData));
                     // We good!
-                    return data.mylist;
+                    return transformedData;
                 }
             },
             onSelect: displayResult
---
+++ src/main/webapp/doc-case-ui/app/mock-ajax.js
@@ -0,0 +1,37 @@
+// Mock an AJAX request
+$.mockjax({
+    url: './rest/documents',
+    response: function() {
+        this.responseText = [{
+            "id": "5557bba230049ab27c8d0d00",
+            "name": "Electricity bill",
+            "scanned": "true",
+            "url": "/rest/file/5557bba230049ab27c8d0cff",
+            "coloured": "true",
+            "signed": "true",
+            "description": "November electricity bill",
+            "type": null,
+            "label": null
+        }, {
+            "id": "5557bda33004155aac98efcf",
+            "name": "Electricity bill1",
+            "scanned": "true",
+            "url": "/rest/file/5557bda33004155aac98efce",
+            "coloured": "true",
+            "signed": "true",
+            "description": "November electricity bill",
+            "type": null,
+            "label": null
+        }, {
+            "id": "5557bee43004155aac98efd1",
+            "name": "Shiv Parivaar",
+            "scanned": "true",
+            "url": "/rest/file/5557bee43004155aac98efd0",
+            "coloured": "true",
+            "signed": "true",
+            "description": "Mahadev Parvati Image",
+            "type": null,
+            "label": null
+        }];
+    }
+});
